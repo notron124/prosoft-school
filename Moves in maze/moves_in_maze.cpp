aa6999f398b24526718006d1bc950254ddf1ff34
Radiy <notron12460@gmail.com>
2025-09-11 23:08:15 +0500

    Добавлено решение для задач Moves in maze и The Labyrinth

---
+++ Moves in maze/moves_in_maze.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+// —оздаем структуру, описывающую нашу €чеку
+// в качестве индикатора о посещении будем использовать '.'
+struct cell {
+    int x, y;
+};
+
+// јлгоритм - стандартна€ интерпретаци€ BFS
+void trace_maze(string *maze, int width, int height, cell c) {
+    // создаем очередь, куда будем складывать €чейки дл€ обработки
+    queue<cell> q;
+    maze[c.y][c.x] = '0'; // сразу отмечаем стартовую позицию
+    q.push(c); // отправл€ем ее в очередь дл€ обработки
+
+    // обрабатываем попавшие в очередь €чейки
+    while(!q.empty()) {
+        // забираем из очереди передний элемент
+        cell curr = q.front();
+        q.pop(); // удал€ем его из очереди
+
+        // описываем все возможные шаги в лабиринте
+        vector<cell> steps = {{(curr.x + 1)%width, curr.y}, // ограничение по ширине, учитыва€ ограничени€ по рамерам такой способ подойдет
+                              {(curr.x - 1) >= 0 ? (curr.x - 1) : (width - 1), curr.y},
+                              {curr.x, (curr.y - 1) >= 0 ? (curr.y - 1) : (height - 1)}, // ограничени€ по высоте
+                              {curr.x, (curr.y + 1)%height}}; 
+
+        // пробуем совершить шаг
+        for (cell step : steps) {
+            // если €чейка доступна дл€ шага
+            if (maze[step.y][step.x] == '.') {
+                // отмечаем номер шага
+                maze[step.y][step.x] = maze[curr.y][curr.x] + 1;
+                if (maze[curr.y][curr.x] == '9') {
+                    maze[step.y][step.x] = 'A';
+                }
+                // отправл€ем эту €чейку в очередь дл€ обработки
+                q.push(step);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int w, h;
+    cell start = {
+        .x = -1,
+        .y = -1,
+    };
+    cin >> w >> h; cin.ignore();
+    string maze[h];
+    // читаем
+    for (int i = 0; i < h; i++) {
+        getline(cin, maze[i]);
+        // сразу находим стартовую позицию 
+        if (start.x < 0) {
+            auto startX = maze[i].find('S');
+            if (startX != string::npos) {
+                start.x = startX;
+                start.y = i;
+            }
+        }
+    }
+
+    // обходим лабиринт
+    trace_maze(maze, w, h, start);
+    for (int i = 0; i < h; i++) {
+        cout << maze[i] << endl;
+    }
+}
---
+++ The labyrinth/the_labyrinth.cpp
@@ -0,0 +1,336 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <climits>
+#include <stack>
+
+using namespace std;
+
+inline const bool IGNORE_CONSOLE = true;
+
+// ƒл€ инверсии шагов во врем€ возврата по уже пройденным €чейкам
+unordered_map<string, string> opposite_steps = {
+    {"RIGHT", "LEFT"},
+    {"LEFT", "RIGHT"},
+    {"UP", "DOWN"},
+    {"DOWN", "UP"}
+};
+
+// »нверси€ очереди
+void reverse_queue(queue<string>& q)
+{
+    stack<string> s;
+    while (!q.empty()) {
+        s.push(q.front());
+        q.pop();
+    }
+    while (!s.empty()) {
+        q.push(s.top());
+        s.pop();
+    }
+}
+
+// »нкапсул€ци€ приоритетной очереди дл€ удобства и читаемости
+template<typename T, typename priority_t>
+struct PriorityQueue {
+    typedef std::pair<priority_t, T> PQElement;
+    std::priority_queue<PQElement, std::vector<PQElement>,
+                    std::greater<PQElement>> elements;
+
+    inline bool empty() const {
+        return elements.empty();
+    }
+
+    inline void put(T item, priority_t priority) {
+        elements.emplace(priority, item);
+    }
+
+    T get() {
+        T best_item = elements.top().second;
+        elements.pop();
+        return best_item;
+    }
+};
+
+// —оздаем структуру, описывающую нашу €чеку
+// в качестве индикатора о посещении будем использовать '.'
+struct cell {
+    int x, y;
+};
+
+// »мплементаци€ hash дл€ cell дл€ возможности напр€мую использовать в unordered_set
+namespace std {
+    template <> struct hash<cell> {
+        std::size_t operator()(const cell& id) const noexcept {
+            return std::hash<int>()(id.x ^ (id.y << 16));
+        }
+    };
+}
+
+
+// ќператоры дл€ структуры cell 
+bool operator == (cell a, cell b) {
+  return a.x == b.x && a.y == b.y;
+}
+
+bool operator != (cell a, cell b) {
+  return !(a == b);
+}
+
+bool operator < (cell a, cell b) {
+  return std::tie(a.x, a.y) < std::tie(b.x, b.y);
+}
+
+// ѕолучить шаг, согласно координатам двух €чеек
+string get_step(cell curr, cell next) {
+    if (next.x > curr.x) {
+        return "RIGHT";
+    } else if (next.x < curr.x) {
+        return "LEFT";
+    } else if (next.y > curr.y) {
+        return "DOWN";
+    } else {
+        return "UP";
+    }
+}
+
+// ѕолучить соседние €чейки
+vector<cell> neighbors(string *maze, cell current, bool ignore_console = false) {
+    vector<cell> possible_steps = {
+        {1, 0}, // right
+        {-1, 0}, // left
+        {0, -1}, // up
+        {0, 1} // down
+    };
+
+    vector<cell> neighbors;
+
+    for (cell step : possible_steps) {
+        cell next_cell = {current.x + step.x, current.y + step.y};
+        char next_char = maze[next_cell.y][next_cell.x];
+        if (next_char == 'C' && !ignore_console) {
+            neighbors.push_back(next_cell);
+            continue;
+        }
+        
+        if (next_char == '.' || next_char == 'T') {
+            neighbors.push_back(next_cell);
+        }
+    }
+
+    return neighbors;
+}
+
+// ‘ункци€, определ€юща€ предположительный "вес" от €чейки до €чейки
+inline int heuristic(cell a, cell b) {
+  return std::abs(a.x - b.x) + std::abs(a.y - b.y);
+}
+
+// Ќайти кратчайший путь €чейка-€чейка
+void find_shortest_way(string *maze, cell start, cell goal, unordered_map<cell, cell> &came_from, unordered_map<cell, int> &cost_so_far) {
+    // создаем приоритетную очередь, куда будем складывать €чейки дл€ обработки
+    PriorityQueue<cell, int> q;
+    q.put(start, 0);
+    cost_so_far[start] = 0;
+
+    // обрабатываем попавшие в очередь €чейки
+    while(!q.empty()) {
+        // забираем из очереди передний элемент
+        cell curr = q.get();
+
+        // ƒосрочный выход из цикла, если дошли до цели
+        if (curr == goal) {
+            break;
+        }
+
+        // пробуем совершить шаг
+        for (cell next : neighbors(maze, curr)) {
+            int new_cost = cost_so_far[curr] + 1;
+            if (cost_so_far.find(next) == cost_so_far.end() || new_cost < cost_so_far[next]) {
+                cost_so_far[next] = new_cost;
+                int priority = new_cost + heuristic(next, goal);
+                came_from[next] = curr;
+                q.put(next, priority);
+            }
+        }
+    }
+}
+
+void explore_maze(string *maze, cell start, unordered_map<cell, string> &came_from) {
+    // создаем приоритетную очередь, куда будем складывать €чейки дл€ обработки
+    queue<cell> q;
+    q.push(start); // отправл€ем ее в очередь дл€ обработки
+
+    // обрабатываем попавшие в очередь €чейки
+    while(!q.empty()) {
+        // забираем из очереди передний элемент
+        cell curr = q.front();
+        q.pop(); // удал€ем его из очереди
+
+        // пробуем совершить шаг
+        for (cell next : neighbors(maze, curr, IGNORE_CONSOLE)) {
+            if (!came_from.contains(next)) {
+                string step = get_step(curr, next);
+
+                came_from[next] = step;
+                q.push(next);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int r; // number of rows.
+    int c; // number of columns.
+    int a; // number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
+    cin >> r >> c >> a; cin.ignore();
+
+    cerr << "ROWS: " << r << " COLUMNS: " << c << endl;
+
+    bool maze_explored = false;
+
+    string maze[r];
+    queue<string> steps;
+
+    cell console_pos = {
+        .x = -1,
+        .y = -1,
+    };
+
+    cell rick_start_pos = {
+        .x = -1,
+        .y = -1,
+    };
+
+    cell rick_pos = {
+        .x = -1,
+        .y = -1,
+    };
+
+    unordered_map<cell, string> current_path;
+
+    // game loop
+    while (1) {
+
+        cell goal;
+        unordered_map<cell, cell> came_from;
+        unordered_map<cell, string> possible_steps;
+        unordered_map<cell, int> cost_so_far;
+
+        int kr; // row where Rick is located.
+        int kc; // column where Rick is located.
+        cin >> kr >> kc; cin.ignore();
+        
+   
+
+        if (rick_start_pos.x == -1) {
+            rick_start_pos.x = kc;
+            rick_start_pos.y = kr;
+        }
+
+        rick_pos.x = kc;
+        rick_pos.y = kr;
+
+        cerr << "RICK POS: " << rick_pos.x << " " << rick_pos.y << endl;
+
+
+        for (int i = 0; i < r; i++) {
+            string row; // C of the characters in '#.TC?' (i.e. one line of the ASCII maze).
+            cin >> row; cin.ignore();
+            cerr << row << endl;
+            maze[i] = row;
+
+            if (console_pos.x == -1) {
+                auto x = row.find('C');
+                if (x != string::npos) {
+                    console_pos.x = x;
+                    console_pos.y = i;
+                }
+            }
+        }
+
+        // Ўагать, пока вектор с шагами не опустеет
+        if (!steps.empty()) {
+            if (rick_pos != console_pos) {
+                cout << steps.front() << endl;
+                steps.pop();
+                continue;
+            } else {
+                steps.pop();
+            }
+        }
+
+        explore_maze(maze, rick_pos, possible_steps);
+
+        // отладка
+        //for (const auto &[key, value]: possible_steps) {
+            //cerr << "This: " << key.x << " " << key.y << " Came from: " << value << endl;
+        //}
+
+        // Ћабиринт исследован, выставить цель на €чейку консоли
+        if (current_path.size() == possible_steps.size() - 1) {
+            maze_explored = true;
+            goal.x = console_pos.x;
+            goal.y = console_pos.y;
+        }
+
+        if (!maze_explored) {
+            cell best_step = {
+                .x = -1,
+                .y = -1,
+            };
+
+            // выбираем не посещенную соседнюю клетку как цель
+            for (auto next : neighbors(maze, rick_pos, IGNORE_CONSOLE)) {
+                cerr << "next: " << next.x << " " << next.y << endl;
+                if (current_path.find(next) == current_path.end()) {
+                    best_step = next;
+                    break;
+                }   
+            }
+
+            // если непосещенных соседних клеток нет, то возвращаемс€
+            if (best_step.x == -1) {
+                cout << current_path[rick_pos] << endl;
+            } else {
+                // отмечаем, что эта клетка посещена
+                cout << possible_steps[best_step] << endl;
+                current_path[best_step] = opposite_steps[possible_steps[best_step]];
+            }
+
+            continue;
+        }
+
+        // ƒобрались до €чейки, выставить цель на выход
+        if (console_pos == rick_pos) {
+            goal.x = rick_start_pos.x;
+            goal.y = rick_start_pos.y; 
+        }
+
+        // Ќайти кратчайший путь
+        find_shortest_way(maze, rick_pos, goal, came_from, cost_so_far);
+
+        cell cur = goal;
+
+        // отладка
+        //for (const auto &[key, value]: came_from) {
+            //cerr << "This: " << key.x << " " << key.y << " Came from: " << value.x << " " << value.y << endl;
+        //}
+
+        // «аполн€ть вектор шагами в обратном пор€дке
+        while (cur != rick_pos) {
+            steps.push(get_step(came_from[cur], cur));
+            cur = came_from[cur];
+        }
+
+        // »нвесри€ вектора
+        reverse_queue(steps);
+
+        // —разу сделать 1 шаг, иначе цикл шагов прерветс€
+        cout << steps.front() << endl;
+        steps.pop();
+    }
+}

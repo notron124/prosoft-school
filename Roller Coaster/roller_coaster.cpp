3e641fa7791eb747797b64e00857feceb7c45fc2
Radiy <notron12460@gmail.com>
2025-09-13 17:17:24 +0500

    Добавлено отслеживание очередей, попавших на аттракцион
    - Отслеживание очередей сделано с целью определения цикличности№

--- Roller Coaster/roller_coaster.cpp
+++ Roller Coaster/roller_coaster.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <unordered_map>
 
 using namespace std;
 
@@ -11,23 +12,77 @@ using namespace std;
  * the standard input according to the problem statement.
  **/
 
+// Определяем метод хеша для std:vercotr<int>, чтобы можно было использовать его в качестве ключа в unordered_map
+namespace std {
+    struct vector_hash {
+        std::size_t operator()(const std::vector<int>& v) const {
+            std::size_t seed = v.size();
+            for (int i : v) {
+                seed ^= std::hash<int>{}(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+            }
+            return seed;
+        }
+    };
+}
+
+
 
 long long calculate_earnings(queue<int> &coaster_queue, const int capacity, const int rides_per_day) {
     long long total_earnings = 0;
+    long long earnings_per_ride = 0;
     const int queue_size = coaster_queue.size();
     int group_cntr = 0;
+    int total_group_cntr = 0;
+    int cap = 0;
+
+    int rides_earnings_prev_size = 0;
+    unordered_map<vector<int>, int, vector_hash> rides_earnings;
+    bool is_cycle_established = false;
+    bool track_queues = true;
+    int rides_to_simulate = 0;
+
     
-    for (int ride = 0; ride < rides_per_day; ride++) {
-        int cap = capacity;
+    for (int ride = rides_per_day; ride > 0; ride--) {
+        vector<int> current_ride_queue;
+        cap = capacity;
+        earnings_per_ride = 0;
         group_cntr = 0;
+
+        if (is_cycle_established && rides_to_simulate > 0) {
+            rides_to_simulate--;
+        } else if (is_cycle_established && total_group_cntr >= queue_size) { // Даем время алгоритму набрать статистику, иначе возможны сработки на неполные данные
+            long long cyclic_earnings = 0;
+            int cyclic_groups_cntr = 0;
+            for (const auto &[q, v] : rides_earnings) {
+                if (v >= 2) {
+                    cyclic_groups_cntr++;
+                    for (int gr : q) {
+                        cyclic_earnings += gr;
+                    }
+                }
+            }
+
+            //cerr << total_earnings << ':' << ride << ':' << cyclic_groups_cntr << ':' << cyclic_earnings << endl;
+            if (cyclic_groups_cntr > 0) {
+                total_earnings += (ride / cyclic_groups_cntr) * cyclic_earnings;
+                ride = ride % cyclic_groups_cntr;
+                if (ride == 0) {
+                    return total_earnings;
+                }
+                is_cycle_established = false;
+                track_queues = false;
+            }
+        }
+
         while (cap > 0) {
             int group = coaster_queue.front();
             cap -= group;
             group_cntr++;
-            
+
             if (cap >= 0) {
+                current_ride_queue.push_back(group);
                 coaster_queue.pop();
-                total_earnings += group;
+                earnings_per_ride += group;
                 coaster_queue.push(group);
             }
 
@@ -35,10 +90,41 @@ long long calculate_earnings(queue<int> &coaster_queue, const int capacity, cons
             if (group_cntr >= queue_size) {
                 break;
             }
-
-            cerr << "cap: " << cap << " group: " << group << endl;
         }
 
+        total_group_cntr += group_cntr;
+
+        // В случае, если вся очередь влезает на аттракцион, просто возвращаем поездки умноженные на выручку с одной поездки
+        if (group_cntr >= queue_size) {
+            return earnings_per_ride * ride;
+        } else {
+            total_earnings += earnings_per_ride;
+        }
+
+        if (!track_queues) {
+            continue;
+        }
+
+        //cerr << "Ride №" << rides_per_day - ride + 1 << endl;
+        rides_earnings[current_ride_queue]++;
+        //for (int i = 0; const auto &[q, v] : rides_earnings) {
+        //    i++;
+        //    cerr << "queue №" << i << " [";
+        //    for (int gr : q) {
+        //        cerr << gr << " ";
+        //    }
+        //    cerr << "] " << ':' << v << endl;
+        //}
+        
+        int curr_re_size = rides_earnings.size();
+        //cerr << curr_re_size << ':' << rides_earnings_prev_size << endl;
+
+        if (curr_re_size > rides_earnings_prev_size) {
+            rides_earnings_prev_size = curr_re_size;
+        } else if (!is_cycle_established){
+            is_cycle_established = true;
+            rides_to_simulate = curr_re_size;
+        }
     }
 
     return total_earnings;

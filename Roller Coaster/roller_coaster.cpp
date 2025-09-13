187d92d5bea7ed3c8049b73088c51fd99bbd6069
Radiy <notron12460@gmail.com>
2025-09-13 13:46:13 +0500

    Коррекция алгоритма для выполения 4го теста
    - Добавлено отслеживание прохождения очереди, чтобы не добавлять группы, когда очередь в очереди никого нет

--- Roller Coaster/roller_coaster.cpp
+++ Roller Coaster/roller_coaster.cpp
@@ -14,17 +14,26 @@ using namespace std;
 
 long long calculate_earnings(queue<int> &coaster_queue, const int capacity, const int rides_per_day) {
     long long total_earnings = 0;
+    const int queue_size = coaster_queue.size();
+    int group_cntr = 0;
     
     for (int ride = 0; ride < rides_per_day; ride++) {
         int cap = capacity;
+        group_cntr = 0;
         while (cap > 0) {
             int group = coaster_queue.front();
             cap -= group;
+            group_cntr++;
             
             if (cap >= 0) {
                 coaster_queue.pop();
                 total_earnings += group;
                 coaster_queue.push(group);
+            }
+
+            // Больше никого нет в очереди
+            if (group_cntr >= queue_size) {
+                break;
             }
 
             cerr << "cap: " << cap << " group: " << group << endl;

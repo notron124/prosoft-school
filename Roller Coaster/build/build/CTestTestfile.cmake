0e2330f3237e9174debc0f6e7385e08770aff5fc
Radiy <notron12460@gmail.com>
2025-09-24 18:45:45 +0500

    Папка build добавлена в gitignore

--- Roller Coaster/build/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/work/prosoft-school/Roller Coaster
-# Build directory: D:/work/prosoft-school/Roller Coaster/build/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")

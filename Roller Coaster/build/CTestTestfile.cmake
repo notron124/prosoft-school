f9f5f7915ad41a3cc34315185d389e3b8a2758d4
Radiy <notron12460@gmail.com>
2025-09-24 18:44:40 +0500

    Добавлены автотесты
    - Добавлены данные для тестов
    
    - Написаны сами тесты непосредственно
    
    - Добавлены CMakeLists файлы

---
+++ Roller Coaster/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/work/prosoft-school/Roller Coaster
+# Build directory: D:/work/prosoft-school/Roller Coaster/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
---
+++ Roller Coaster/inc/roller_coaster.h
@@ -0,0 +1,7 @@
+#ifndef ROLLER_COASTER_H_
+#define ROLLER_COASTER_H_
+#include <queue>
+
+long long calculate_earnings(std::queue<int> &coaster_queue, int capacity, int rides);
+
+#endif /* ROLLER_COASTER_H_ */
